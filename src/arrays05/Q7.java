1fc42ba344e1ae3c0944dc6756308b3e0b610b6f
suyog2001 <suyog0057@gmail.com>
2022-02-06 00:30:00 +0530

    Q7 Q8 solved Arrays05

---
+++ src/arrays05/Q7.java
@@ -0,0 +1,40 @@
+package arrays05;
+
+public class Q7 {
+    // ? https://leetcode.com/problems/number-of-good-pairs/
+    public static void main(String[] args) {
+        int[] input = new int[] { 1, 2, 3, 1, 1, 3 };
+        // ex: input: [1,1,1,1] output : 6
+        // System.out.println("Output : " + (new Q7()).numIdenticalPairsMethod1(input));
+        System.out.println("Output : " + (new Q7()).numIdenticalPairsMethod2(input));
+    }
+
+    public int numIdenticalPairsMethod1(int[] nums) {
+        int pair = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j) {
+                    pair++;
+                }
+            }
+        }
+        return pair;
+    }
+
+    // * optimal Solution
+    /**
+     * Constrains in the question 
+     * 1 <= nums.length <= 100,
+     * 1 <= nums[i] <= 100
+     */
+    public static int numIdenticalPairsMethod2(int[] nums) {
+
+        int result = 0;
+        int[] constArray = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            result += constArray[nums[i]];
+            constArray[nums[i]]++;
+        }
+        return result;
+    }
+}
---
+++ src/arrays05/Q8.java
@@ -0,0 +1,53 @@
+package arrays05;
+
+import java.security.DrbgParameters.Reseed;
+import java.util.Arrays;
+
+public class Q8 {
+    // ?
+    // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    public static void main(String[] args) {
+        int[] inputArray = new int[] { 8, 1, 2, 2, 3 };
+        System.out.println("output : " + Arrays.toString((new Q8()).smallerNumbersThanCurrentMethod2(inputArray)));
+    }
+
+    // ?Brute force
+    public int[] smallerNumbersThanCurrentMethod1(int[] nums) {
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j] && i != j) {
+                    result[i]++;
+                }
+            }
+        }
+        return result;
+    }
+
+    // * Optimal Question
+    /**
+     * Constains
+     * 2 <= nums.length <= 500
+     * 0 <= nums[i] <= 100
+     */
+    public int[] smallerNumbersThanCurrentMethod2(int[] nums) {
+        int[] temp = new int[101];
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[nums[i]]++;
+        }
+        for (int i = 1; i < 101; i++) {
+            temp[i] = temp[i] + temp[i - 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                nums[i] = 0;
+
+            } else {
+                result[i] = temp[nums[i] - 1];
+            }
+        }
+        return result;
+    }
+}
