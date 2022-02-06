2646de6dd906761f099ca231ed46faac5712398b
suyog2001 <suyog0057@gmail.com>
2022-02-06 23:44:34 +0530

    Array Q9

---
+++ src/DSABootcampJava/arrays05/Q9.java
@@ -0,0 +1,45 @@
+package DSABootcampJava.arrays05;
+
+import java.util.Arrays;
+
+import javax.sound.sampled.TargetDataLine;
+
+public class Q9 {
+    public static void main(String[] args) {
+        // Both Array of same Length
+        int arr[] = new int[] { 0, 1, 2, 3, 4 };
+        // { 0, 1, 0 };
+        int index[] = new int[] { 0, 1, 2, 2, 1 };
+        // { 0, 1, 0 };
+        System.out.println("Output: " + Arrays.toString(createTargetArray(arr, index)));
+    }
+
+    /**
+     * Constrains
+     * 1 <= nums.length, index.length <= 100
+     * nums.length == index.length
+     * 0 <= nums[i] <= 100
+     * 0 <= index[i] <= i
+     * 
+     * @param nums
+     * @param index
+     * @return
+     */
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        int target[] = new int[index.length];
+        for (int i = 0; i < index.length; i++) {
+            target[i] = -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (target[index[i]] != -1) {
+                for (int j = i; j > index[i]; j--) {
+                    target[j] = target[j - 1];
+                }
+            }
+            target[index[i]] = nums[i];
+
+        }
+
+        return target;
+    }
+}
